d653a9ceaa6e1ad194a1b1c631b9039866717de1
Travis Turner <roboenigma@gmail.com>
2018-01-17 12:42:27 -0500

    Add CORS header

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   let formattedHTML = '';
   let name = req.body.name;
   let email = req.body.email;
