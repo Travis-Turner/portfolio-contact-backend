7e1380ead6cce3fffff71914ef515167e172ae43
Travis Turner <roboenigma@gmail.com>
2018-01-17 11:23:59 -0500

    Set up POST route, nodemailer

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 var app = express();
+
+const transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: process.env.USER_NAME,
+        pass: process.env.USER_CUE
+    }
+});
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send('test.');
+  res.send('Welcome.');
 });
 
 app.post('/', (req, res) => {
-  res.send(req.body);
+  let name = req.body.name;
+  let email = req.body.email;
+  let phone = req.body.phone;
+  let message = req.body.message;
+
+  let formattedHTML = '<h1>FROM: ' + name + '</h1>';
+  formattedHTML += '<h3>EMAIL: ' + email + '</h3>';
+  formattedHTML += '<h3>PHONE: ' + phone + '</h3>';
+  formattedHTML += '<p>MESSAGE: ' + message + '</p>';
+
+  const mailOptions = {
+    from: 'travisturner00000@gmail.com',
+    to: 'travis@travisturner.io',
+    subject: 'TRAVIS CONTACT FORM SUBMISSION',
+    html: formattedHTML
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+   });
+   res.send('done');
 });
 
 app.listen(port, () => {
   console.log('Server up.');
+  console.log(process.env.USER_NAME);
+  console.log(process.env.USER_CUE);
 });
