9ae98634eab3e343cabd83ba6c52a5b5e3fb4bf8
Travis Turner <roboenigma@gmail.com>
2018-01-17 09:40:36 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('test.');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log('Server up.');
+});
