d9498b76eaa6db90ac50678b237fb2ed8f513c82
Travis Turner <roboenigma@gmail.com>
2018-01-24 15:29:49 -0500

    Add client route

--- app.js
+++ app.js
@@ -64,8 +64,47 @@ app.post('/', (req, res) => {
    });
 });
 
+app.post('/rick', (req, res) => {
+  if (!req.body.name){
+    res.send('Fantastic error.');
+  }
+  let formattedHTML = '';
+  let name = req.body.name;
+  let email = req.body.email;
+  let message = req.body.message;
+
+  formattedHTML = '<h1>FROM: ' + name + '</h1>';
+  //handle optional fields
+  if (req.body.phone){
+    let phone = req.body.phone;
+    formattedHTML += '<h3>PHONE: ' + phone + '</h3>';
+  }
+  if (req.body.website){
+    let website = req.body.website;
+    formattedHTML += '<h3>WEBSITE: ' + website + '</h3>';
+  }
+  formattedHTML += '<h3>EMAIL: ' + email + '</h3>';
+  formattedHTML += '<p>MESSAGE: ' + message + '</p>';
+
+  const mailOptions = {
+    from: process.env.FROM_EMAIL,
+    to: process.env.RICK_EMAIL,
+    subject: 'MESSAGE FROM RICK\'S CARPENTRY',
+    html: formattedHTML
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+     if(err) {
+      res.send('Error!');
+     }
+     else {
+       console.log(info);
+       backURL=req.header('Referer') || '/';
+       res.redirect(backURL);
+     }
+   });
+});
+
 app.listen(port, () => {
   console.log('Server up.');
-  console.log(process.env.USER_NAME);
-  console.log(process.env.USER_CUE);
 });
